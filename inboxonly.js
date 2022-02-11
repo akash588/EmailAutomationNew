71e3f976b59b563dcea921b91d95435c87e1159c
akash shelar <akash.techinvento@gmail.com>
2022-02-11 12:16:44 +0530

    dir added

--- inboxonly.js
+++ inboxonly.js
@@ -49,11 +49,15 @@ const gmailProcess = async () => {
       xvfb_args: ["-screen", "0", '1280x720x24', "-ac"],
   });
   xvfb.start((err)=>{if (err) console.error(err)})
-  // const dir = './1'
+  const dir = './1'
    
   
   
   
+
+fs.readdirSync(dir).forEach(file => {
+  console.log(file);
+});
     const sessionData = path.resolve(__dirname, './1') ;
     // const folderPath = '/1'
     // fs.readdirSync(folderPath)
