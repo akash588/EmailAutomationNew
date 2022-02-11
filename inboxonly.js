441c34f272e5cc654e52a244405267077baf0b80
akash shelar <akash.techinvento@gmail.com>
2022-02-11 11:20:19 +0530

    dir changes

--- inboxonly.js
+++ inboxonly.js
@@ -49,8 +49,12 @@ const gmailProcess = async () => {
       xvfb_args: ["-screen", "0", '1280x720x24', "-ac"],
   });
   xvfb.start((err)=>{if (err) console.error(err)})
- 
-    const sessionData = path.join('1');;
+  const dir = './1'
+   
+  
+  
+  
+    const sessionData = dir;
     // const folderPath = '/1'
     // fs.readdirSync(folderPath)
     const args = [
